1db9af56f671a02c56bfc8fbd8973d6105374747
Jamie Gunn <your-email@example.com>
2026-06-27 14:51:55 +0100

    Hero images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
       {/* Background image */}
       <div className="absolute inset-0">
         <Image
-          src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=2000&q=80"
+          src="images/Hero.gif"
           alt="Luxury bell tent in countryside"
           fill
           priority
